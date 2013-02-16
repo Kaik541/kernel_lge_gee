4746d99e381da5c5d0bda55be2832da2135d7fd4
synergydev <synergy@spentlemon.com>
2013-02-16 00:07:20 +0000

    drivers: gpu: import from jb-mr1.1 mako kernel

--- drivers/video/msm/vidc/.vidc_vdec.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/video/msm/vidc/vidc_vdec.o := /home/synergy/ssd_storage/cfx-mako/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-ld -EL    -r -o drivers/video/msm/vidc/vidc_vdec.o drivers/video/msm/vidc/common/dec/vdec.o 
