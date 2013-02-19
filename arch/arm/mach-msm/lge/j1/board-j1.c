9f4a57402c408cbbfb7836e358239ec5cb255f21
synergydev <synergy@spentlemon.com>
2013-02-19 20:54:46 +0000

    qseecom changes from mako jb-mr1.1-release

--- arch/arm/mach-msm/lge/j1/board-j1.c
+++ arch/arm/mach-msm/lge/j1/board-j1.c
@@ -1826,16 +1826,10 @@ static struct platform_device msm_device_iris_fm __devinitdata = {
 /* qseecom bus scaling */
 static struct msm_bus_vectors qseecom_clks_init_vectors[] = {
         {
-                .src = MSM_BUS_MASTER_ADM_PORT0,
+                .src = MSM_BUS_MASTER_SPS,
                 .dst = MSM_BUS_SLAVE_EBI_CH0,
-                .ab = 0,
                 .ib = 0,
-        },
-        {
-                .src = MSM_BUS_MASTER_ADM_PORT1,
-                .dst = MSM_BUS_SLAVE_GSBI1_UART,
                 .ab = 0,
-                .ib = 0,
         },
         {
                 .src = MSM_BUS_MASTER_SPDM,
@@ -1847,16 +1841,10 @@ static struct msm_bus_vectors qseecom_clks_init_vectors[] = {
 
 static struct msm_bus_vectors qseecom_enable_dfab_vectors[] = {
         {
-                .src = MSM_BUS_MASTER_ADM_PORT0,
+                .src = MSM_BUS_MASTER_SPS,
                 .dst = MSM_BUS_SLAVE_EBI_CH0,
-                .ab = 70000000UL,
-                .ib = 70000000UL,
-        },
-        {
-                .src = MSM_BUS_MASTER_ADM_PORT1,
-                .dst = MSM_BUS_SLAVE_GSBI1_UART,
-                .ab = 2480000000UL,
-                .ib = 2480000000UL,
+                .ib = (492 * 8) * 1000000UL,
+                .ab = (492 * 8) *  100000UL,
         },
         {
                 .src = MSM_BUS_MASTER_SPDM,
@@ -1868,16 +1856,10 @@ static struct msm_bus_vectors qseecom_enable_dfab_vectors[] = {
 
 static struct msm_bus_vectors qseecom_enable_sfpb_vectors[] = {
         {
-                .src = MSM_BUS_MASTER_ADM_PORT0,
+                .src = MSM_BUS_MASTER_SPS,
                 .dst = MSM_BUS_SLAVE_EBI_CH0,
-                .ab = 0,
                 .ib = 0,
-        },
-        {
-                .src = MSM_BUS_MASTER_ADM_PORT1,
-                .dst = MSM_BUS_SLAVE_GSBI1_UART,
                 .ab = 0,
-                .ib = 0,
         },
         {
                 .src = MSM_BUS_MASTER_SPDM,
@@ -1888,32 +1870,32 @@ static struct msm_bus_vectors qseecom_enable_sfpb_vectors[] = {
 };
 
 static struct msm_bus_paths qseecom_hw_bus_scale_usecases[] = {
-	{
-		ARRAY_SIZE(qseecom_clks_init_vectors),
-		qseecom_clks_init_vectors,
-	},
-	{
-		ARRAY_SIZE(qseecom_enable_dfab_vectors),
-                qseecom_enable_dfab_vectors,
-	},
-	{
-		ARRAY_SIZE(qseecom_enable_sfpb_vectors),
-		qseecom_enable_sfpb_vectors,
-	},
+        {
+                ARRAY_SIZE(qseecom_clks_init_vectors),
+                qseecom_clks_init_vectors,
+        },
+        {
+                ARRAY_SIZE(qseecom_enable_dfab_vectors),
+                qseecom_enable_sfpb_vectors,
+        },
+        {
+                ARRAY_SIZE(qseecom_enable_sfpb_vectors),
+                qseecom_enable_sfpb_vectors,
+        },
 };
 
 static struct msm_bus_scale_pdata qseecom_bus_pdata = {
-	qseecom_hw_bus_scale_usecases,
-	ARRAY_SIZE(qseecom_hw_bus_scale_usecases),
-	.name = "qsee",
+        qseecom_hw_bus_scale_usecases,
+        ARRAY_SIZE(qseecom_hw_bus_scale_usecases),
+        .name = "qsee",
 };
 
 static struct platform_device qseecom_device = {
-	.name		= "qseecom",
-	.id		= 0,
-	.dev		= {
-		.platform_data = &qseecom_bus_pdata,
-	},
+        .name           = "qseecom",
+        .id             = 0,
+        .dev            = {
+                .platform_data = &qseecom_bus_pdata,
+        },
 };
 #endif
 
